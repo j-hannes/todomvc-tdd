33a2e1e7d4650c600b0c7eaf8e4910cd9a2f4dcc
Jo Hannes <hannes_e@gmx.de>
2013-10-05 19:12:47 +0100

    App.render() should append template to #todoapp

--- test/spec/views/app-view-spec.js
+++ test/spec/views/app-view-spec.js
@@ -1,16 +1,30 @@
-/* global define, describe, it, expect */
+/* global define, describe, it, expect, beforeEach, afterEach */
 
 define([
+  'jquery',
   'views/app',
-  'jasmineJquery'
-], function(App) {
+  'jasmineJquery',
+], function($, App) {
   'use strict';
  
   describe('View :: App', function() {
     describe('render()', function() {
-      it('returns the view object', function() {
+      beforeEach(function() {
+        $('body').append($('<div id="todoapp"></div>'));
         this.app = new App();
+      });
+      afterEach(function() {
+        this.app.remove();
+        $('#todoapp').remove();
+      });
+      it('returns the view object', function() {
         expect(this.app.render()).toEqual(this.app);
+      });
+      it('appends the template content to a #todoapp container', function() {
+        this.app.render();
+        expect($('#todoapp')).toContain('header#header');
+        expect($('#todoapp')).toContain('section#main');
+        expect($('#todoapp')).toContain('footer#footer');
       });
     });
   });
