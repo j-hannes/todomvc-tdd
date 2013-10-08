b59452475ceb31d8d5ef9e82863cfe77ffebf138
Jo Hannes <hannes_e@gmx.de>
2013-10-08 12:23:08 +0100

    Create a new todo model on entering a todo

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -5,14 +5,19 @@ define([
   'underscore',
   'backbone',
   'templates',
+  'models/todo',
   'collections/todo'
-], function($, _, Backbone, JST, TodoCollection) {
+], function($, _, Backbone, JST, Todo, TodoCollection) {
   'use strict';
 
   var AppView = Backbone.View.extend({
     template: JST['app/scripts/templates/app.ejs'],
 
     el: '#todoapp',
+
+    events: {
+      'keypress #new-todo': 'createOnEnter'
+    },
 
     initialize: function() {
       this.collection = new TodoCollection();
@@ -21,6 +26,10 @@ define([
     render: function() {
       this.$el.append(this.template());
       return this;
+    },
+
+    createOnEnter: function() {
+      this.collection.add(new Todo());
     }
   });
 
