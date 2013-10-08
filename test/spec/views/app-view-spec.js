34d11132e8b323e4d4aa5ff187744e936637f337
Jo Hannes <hannes_e@gmx.de>
2013-10-08 09:34:27 +0100

    Place DOM anchor for app in the index HTML file

--- test/spec/views/app-view-spec.js
+++ test/spec/views/app-view-spec.js
@@ -3,25 +3,27 @@
 define([
   'jquery',
   'views/app',
-  'jasmineJquery',
+  'jasmineJquery'
 ], function($, App) {
   'use strict';
  
   describe('View :: App', function() {
     describe('render()', function() {
       beforeEach(function() {
-        $('body').append($('<div id="todoapp"></div>'));
         this.app = new App();
+        this.app.render();
       });
+
       afterEach(function() {
         this.app.remove();
-        $('#todoapp').remove();
+        $('body').append('<section id="todoapp"></section>');
       });
+
       it('returns the view object', function() {
         expect(this.app.render()).toEqual(this.app);
       });
+
       it('appends the template content to a #todoapp container', function() {
-        this.app.render();
         expect($('#todoapp')).toContain('header#header');
         expect($('#todoapp')).toContain('section#main');
         expect($('#todoapp')).toContain('footer#footer');
