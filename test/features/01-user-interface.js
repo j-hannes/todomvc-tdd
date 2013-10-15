17d7a521a9f0ce8d0d00fca97f5ba1e450cf01d9
J-Hannes <Hannes_E@gmx.de>
2013-10-15 15:06:58 +0100

    Add feature "user interface"

---
+++ test/features/01-user-interface.js
@@ -0,0 +1,25 @@
+/* global define, describe, it, expect */
+
+define([
+  'router/todo-app',
+  'jasmineJquery'
+], function(TodoApp) {
+  'use strict';
+
+  describe('Given I have opened a browser', function() {
+    // no action necessary, it is assumed the browser is open
+
+    describe('When I have loaded the application', function() {
+      var app = new TodoApp();
+      app.start();
+
+      it('Then I should see the input for new todos', function() {
+        expect($('body')).toContain('input#new-todo');
+      });
+
+      it('And I should see the todo list', function() {
+        expect($('body')).toContain('ul#todo-list');
+      });
+    });
+  });
+});
