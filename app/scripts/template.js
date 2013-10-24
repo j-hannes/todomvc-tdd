5ede33bf48a06290f019f8c2ab59223acd858adb
J-Hannes <Hannes_E@gmx.de>
2013-10-24 09:54:39 +0100

    Update template and story roadmap

--- app/scripts/template.js
+++ app/scripts/template.js
@@ -138,7 +138,7 @@ app.TodoView = Backbone.View.extend({
 
   initialize: function() {
     // this.listenTo(this.model, 'change', this.render);
-    this.listenTo(this.model, 'destroy', this.remove);
+    // this.listenTo(this.model, 'destroy', this.remove);
     this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
